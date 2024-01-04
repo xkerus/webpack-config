09893c6bed628b7d6e7e09431a65140e3235e843
kerus <xkerus@gmail.com>
2024-01-04 15:24:30 +0200

    Css modules

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import './App.scss'
+import classes from './App.module.scss';
 export const App = () => {
     const [count, setCount] = useState<number>(0);
 
@@ -7,8 +7,8 @@ export const App = () => {
     return (
         <div>
             hello world!
-            <h1>{count}</h1>
-            <button onClick={increment}><span>inc</span></button>
+            <h1 className={classes.value}>{count}</h1>
+            <button className={classes.button} onClick={increment}><span>inc</span></button>
         </div>
     );
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -11,7 +11,7 @@ interface EnvVariables {
 
 export default (env: EnvVariables) => {
     const paths: BuildPaths = {
-        output: path.resolve(__dirname, 'buildConfig'),
+        output: path.resolve(__dirname, 'build'),
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
         html: path.resolve(__dirname, 'public', 'index.html'),
     }
