208b210ea9cea191f87a4dc6969249108c0e0569
kerus <xkerus@gmail.com>
2024-01-05 17:01:55 +0200

    assets

--- buildConfig/buildLoaders.ts
+++ buildConfig/buildLoaders.ts
@@ -5,14 +5,43 @@ import {BuildOptions} from "./types/types";
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
 
-    const cssLoaderWithModules = {
-        loader: "css-loader",
-        options: {
-            modules: {
-                localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
-            },
-        },
+    const assetLoader = {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
     }
+
+    const svgLoader = {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    icon: true,
+                    svgoConfig: {
+                        plugins: [
+                            {
+                                name: 'convertColors',
+                                params: {
+                                    currentColor: true,
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        ],
+    }
+
+    const
+        cssLoaderWithModules = {
+            loader: "css-loader",
+            options: {
+                modules: {
+                    localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+                },
+            },
+        }
 
     const scssLoader = {
         test: /\.s[ac]ss$/i,
@@ -33,7 +62,9 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     }
 
     return [
+        assetLoader,
         scssLoader,
         tsLoader,
+        svgLoader,
     ]
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,18 +2,30 @@ import React, {useState} from 'react';
 import classes from './App.module.scss';
 import {Link, Outlet} from "react-router-dom";
 import About from "@/pages/about/About";
+import catPng from "@/assets/rizhov.png";
+import catJng from "@/assets/Cat.jpg";
+import Shield from "@/assets/shield.svg";
+
 export const App = () => {
     const [count, setCount] = useState<number>(0);
 
     const increment = () => setCount(prev => prev + 1)
     return (
         <div>
+            <div>
+                <img width={250} src={catPng} alt="cat"/>
+                <img width={250} src={catJng} alt="cat"/>
+            </div>
+            <div>
+                <Shield className={classes.icon} width={100} height={100}/>
+            </div>
             <Link to={'/about'}>about</Link>
             <Link to={'/shop'}>shop</Link>
             hello world!
             <h1 className={classes.value}>{count}</h1>
             <button className={classes.button} onClick={increment}><span>inc</span></button>
-            <About />
+            <About/>
+            <Outlet/>
         </div>
     );
 };
